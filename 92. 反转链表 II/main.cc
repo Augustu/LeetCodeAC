e45caf78de201726d5a15343bf2720d0a41e4ca5
ycx <ycx@ulang.com>
2020-11-10 16:35:44 +0800

    add to solve 92 reverseBetween

--- 92. 反转链表 II/main.cc
+++ 92. 反转链表 II/main.cc
@@ -8,5 +8,21 @@ using namespace std;
 typedef LinkNode<int> ListNode;
 
 
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        // TODO solve this
+    }
+};
 
 
+int main() {
+    ListNode* test_1 = makeLinkedList(1,2,3,4,5);
+
+    ListNode* result;
+    Solution s;
+    result = s.reverseBetween(test_1, 2, 4);
+    printLinkedList(result);
+
+    return 0;
+}
