ffeb4c2f2745de81673513d1aeba739a3791ad75
ycx <ycx@ulang.com>
2020-11-11 17:08:54 +0800

    add AC N141

---
+++ 141. 环形链表/main.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "linkedlist"
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr || head->next == nullptr) return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (slow != fast) {
+            if (fast == nullptr || fast->next == nullptr) {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return true;
+    }
+};
+
+
+int main() {
+    return 0;
+}
--- 92. 反转链表 II/main.cc
+++ 92. 反转链表 II/main.cc
@@ -5,13 +5,84 @@
 using namespace std;
 
 
-typedef LinkNode<int> ListNode;
+/*
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if (!head) return head;
+
+        ListNode* node = new ListNode(-1, head);
+        head = node;
+        int pos = 1;
+        while (node) {
+            // when equal the next node is the beginning to be reversed
+            if (pos == m) {
+                node->next = reverseFromHead(node->next, n-m+1);
+                break;
+            }
+
+            node = node->next;
+            pos++;
+        }
+
+        return head->next;
+    }
+
+    ListNode* reverseFromHead(ListNode* head, int length) {
+        ListNode* pre = nullptr;
+        ListNode* post = nullptr;
+        ListNode *current = head;
+        while (length-- && current) {
+            post = current->next;
+            current->next = pre;
+            pre = current;
+            current = post;
+        }
+        head->next = current;
+        return pre;
+    }
+};
+*/
 
 
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int m, int n) {
-        // TODO solve this
+        ListNode* sentinal = new ListNode(-1, head);
+        ListNode* pre = sentinal;
+        for (int i=1; i<m; ++i) {
+            pre = pre->next;
+        }
+
+        head = pre->next;
+
+        /* 1. pre -> head -> post -> follow
+         * 
+         * 2. pre -> head    post -> follow
+         *            └----------------┘
+         * 
+         *            ┌-------┐
+         *            ↓       |
+         * 3. pre -> head    post -> follow
+         *            │                ↑
+         *            └----------------┘
+         * 
+         *             ┌-------┐
+         *             ↓       |
+         * 4. pre    head    post -> follow
+         *     │       │      ↑         ↑
+         *     |       └------┼---------┘
+         *     └--------------┘
+         * 
+        */ 
+        for (int i=m; i<n; ++i) {
+            ListNode* post = head->next;
+            head->next = post->next; // 2
+            post->next = pre->next; // 3
+            pre->next = post; // 4
+        }
+
+        return sentinal->next;
     }
 };
 
@@ -22,6 +93,10 @@ int main() {
     ListNode* result;
     Solution s;
     result = s.reverseBetween(test_1, 2, 4);
+    printLinkedList(result);
+    result = s.reverseBetween(result, 1, 4);
+    printLinkedList(result);
+    result = s.reverseBetween(result, 3, 5);
     printLinkedList(result);
 
     return 0;
