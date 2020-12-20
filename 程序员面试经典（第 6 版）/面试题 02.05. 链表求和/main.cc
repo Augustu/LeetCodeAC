071dbdce2bbaeb04e8de1bc1bbf596319d74d913
Mike <mike@Augustu.localhost>
2020-12-20 14:42:55 +0800

    beautify 02.05

--- 程序员面试经典（第 6 版）/面试题 02.05. 链表求和/main.cc
+++ 程序员面试经典（第 6 版）/面试题 02.05. 链表求和/main.cc
@@ -5,54 +5,80 @@
 using namespace std;
 
 
+// class Solution {
+// public:
+//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//         ListNode* head = l1;
+//         ListNode* prev = nullptr;
+//         int carry = 0;
+//         while (l1 && l2) {
+//             int tmp = l1->val + l2->val + carry;
+//             l1->val = tmp % 10;
+//             carry = tmp / 10;
+
+//             prev = l1;
+//             l1 = l1->next;
+//             l2 = l2->next;
+//         }
+
+//         if (l1) {
+//             while (l1) {
+//                 int tmp = l1->val + carry;
+//                 l1->val = tmp % 10;
+//                 carry = tmp / 10;
+//                 prev = l1;
+//                 l1 = l1->next;
+//             }
+//         }
+//         if (l2) {
+//             while (l2) {
+//                 int tmp = l2->val + carry;
+//                 l2->val = tmp % 10;
+//                 carry = tmp / 10;
+//                 prev->next = l2;
+//                 prev = l2;
+//                 l2 = l2->next;
+//             }
+//         }
+
+//         if (carry) {
+//             prev->next = new ListNode(carry);
+//         }
+
+//         return head;
+//     }
+// };
+
+
+// beautify the if checks, make if into little blocks
+// https://leetcode-cn.com/submissions/detail/123510101/
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* head = l1;
-        ListNode* prev = nullptr;
+        ListNode* sentinel = new ListNode(-1);
+        ListNode* current = sentinel;
+
         int carry = 0;
-        while (l1 && l2) {
-            int tmp = l1->val + l2->val + carry;
-            l1->val = tmp % 10;
-            carry = tmp / 10;
-
-            prev = l1;
-            l1 = l1->next;
-            l2 = l2->next;
-        }
-
-        if (l1) {
-            while (l1) {
-                int tmp = l1->val + carry;
-                l1->val = tmp % 10;
-                carry = tmp / 10;
-                prev = l1;
+        while (l1 || l2 || carry) {
+            int a = 0, b = 0;
+            if (l1) {
+                a = l1->val;
                 l1 = l1->next;
             }
-        }
-        if (l2) {
-            while (l2) {
-                int tmp = l2->val + carry;
-                l2->val = tmp % 10;
-                carry = tmp / 10;
-                prev->next = l2;
-                prev = l2;
+            if (l2) {
+                b = l2->val;
                 l2 = l2->next;
             }
+
+            int tmp = a + b + carry;
+            current->next = new ListNode(tmp % 10);
+            current = current->next;
+            carry = tmp / 10;
         }
 
-        if (carry) {
-            prev->next = new ListNode(carry);
-        }
-
-        return head;
+        return sentinel->next;
     }
 };
-
-
-// TODO beautify the if checks, make if into little blocks
-// https://leetcode-cn.com/submissions/detail/123510101/
-
 
 int main() {
     return 0;
